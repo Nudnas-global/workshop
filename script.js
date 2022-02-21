f42c361d34fe68e7054333b959444ff754ab9c10
Nudnas <30370385+Nudnas-global@users.noreply.github.com>
2022-02-21 19:42:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -20,7 +20,20 @@ function initMap(){
         title: "University"
 
     });
-
-
+    
+    const MyRouteCoordinates = [
+        { lat: 6.796059858230645, lng: 79.90082888636698 },
+        { lat: 6.793667946401544, lng: 79.90072062428649 },
+        { lat: 6.793282731024652, lng: 79.90078377716678 },
+        { lat: 6.7913118568276944, lng: 79.90125291284883 },
+      ];
+    const MyPath = new google.maps.Polyline({
+        path: MyRouteCoordinates,
+        geodesic: true,
+        strokeColor: "#9B5094",
+        strokeOpacity: 1.0,
+        strokeWeight: 2,
+      });
+      MyPath.setMap(map);
 
 }
